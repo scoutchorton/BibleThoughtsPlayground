89a97eb2beb4a58a5eaaaa02590ef0274b04d5ee
scoutchorton <scoutchorton@yahoo.com>
2018-08-11 16:43:45 -0400

    First post editor update. Lists posts with no functions.

--- author/loginScripts.js
+++ author/loginScripts.js
@@ -1,5 +1,6 @@
 var a=true;
-function eId(i){return document.getElementById(i)}
+function eId(i){return document.getElementById(i);}
+function fade(id){eId(id).classList.add('fdIn');setTimeout(function(){eId(id).classList.remove('fdIn');},5000);}
 firebase.initializeApp({apiKey:"AIzaSyC0q4W7LycPoJJQ7d6iluAyDxg5iNS3Jx4",authDomain:"github-page-demo.firebaseapp.com",databaseURL:"https://github-page-demo.firebaseio.com",projectId:"github-page-demo",storageBucket:"github-page-demo.appspot.com",messagingSenderId:"608313227028"});
 var db=firebase.database().ref();
 db.child("/BibleThoughts/admins").on('value',function(s){eId("aData").innerHTML=JSON.stringify(s.val());});
@@ -13,12 +14,12 @@ function signIn(){
 				window.location.href="./author.html";
 			}else{
 				eId('spacer').innerHTML="Not an author account. Please email scoutchorton@gmail.com if you believe this is false.";
-				setTimeout(function(){eId('spacer').innerHTML="";},4500);
+				fade('spacer');
 				a=false;
 				firebase.auth().signOut();
 				eId('loading').style.display='none';
 			}
 		}
 	)
-	.catch(function(e) {var er={"auth/wrong-password":"Incorrect password for this user.","auth/user-not-found":"No user found for this email.","auth/user-disabled":"Account deactivated/disabled. Email scoutchorton@gmail.com to resolve this issue.","auth/invalid-email":"Email not a valid email address."};eId('spacer').innerHTML=er[e.code];setTimeout(function(){eId('spacer').innerHTML="";},4500);eId('loading').style.display='none';});}
+	.catch(function(e) {var er={"auth/wrong-password":"Incorrect password for this user.","auth/user-not-found":"No user found for this email.","auth/user-disabled":"Account deactivated/disabled. Email scoutchorton@gmail.com to resolve this issue.","auth/invalid-email":"Email not a valid email address."};eId('spacer').innerHTML=er[e.code];fade('spacer');eId('loading').style.display='none';});}
 window.onload=function(){eId('loading').style.display='block';firebase.auth().onAuthStateChanged(function(user){if(user&&a){window.location.href="./author.html";}else{eId('loading').style.display='none';}});eId('password').addEventListener("keyup",function(e){e.preventDefault();if(e.keyCode==13){eId('loginButton').click();}});}
--- author/scripts.js
+++ author/scripts.js
@@ -2,8 +2,20 @@
 function eId(i){return document.getElementById(i)};
 // Initialize Firebase
 console.clear();var config={apiKey:"AIzaSyC0q4W7LycPoJJQ7d6iluAyDxg5iNS3Jx4",authDomain:"github-page-demo.firebaseapp.com",databaseURL:"https://github-page-demo.firebaseio.com",projectId:"github-page-demo",storageBucket:"github-page-demo.appspot.com",messagingSenderId:"608313227028"};firebase.initializeApp(config);
+var db=firebase.database().ref();
 //Functions
 function toggleActive(id){if(eId(id).classList.contains('activeSetting')){eId(id).classList.remove('activeSetting');}else{eId(id).classList.add('activeSetting');eId(id)}}
-window.onload = function(){firebase.auth().onAuthStateChanged(function(user){if(user){eId('greeting').innerHTML=eId('greeting').innerHTML.replace("?user?",firebase.auth().currentUser.displayName);}else{eId('greeting').innerHTML="Redirecting to login page...";window.location.href="./login.html";}});};
+function setFrame(addr){eId('wkspFrame').src=addr;}
+function greet(){firebase.auth().onAuthStateChanged(function(user){if(user){eId('greeting').innerHTML=eId('greeting').innerHTML.replace("?user?",user.displayName);}});};
 function c(){firebase.auth().signOut();}
-function updateDisplayName(n){console.log("Updating display name for: "+firebase.auth().currentUser);firebase.auth().currentUser.updateProfile({displayName:n}).then(function(){alert("Display name updated!");}).catch(function(e){alert(e);})}
+//function updateDisplayName(n){console.log("Updating display name for: "+firebase.auth().currentUser);firebase.auth().currentUser.updateProfile({displayName:n}).then(function(){alert("Display name updated!");}).catch(function(e){alert(e);})}
+/*db.child('/BibleThoughts/posts/').on('value',function(s){
+	var p=[];var d=s.val();for(var i=0;i<Object.keys(d).length;i++){if(firebase.auth().currentUser.uid==Object.keys(d[i])[0]){p.push(d[i]);}eId('fbData').innerHTML=JSON.stringify(p);}
+	eId('postsContainer').innerHTML='';
+	for(var i=0;i<Object.keys(p).length;i++){
+		console.log(Object.keys(p[i][Object.keys(p[i])[0]])[0]);
+		var l=document.createElement('li');
+		l.innerHTML="<i class=\"material-icons\">chevron_right</i>"+Object.keys(p[i][Object.keys(p[i])[0]])[0];
+		eId('postsContainer').appendChild(l);
+	}
+});*/
